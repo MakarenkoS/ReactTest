078b89f68925e3f74d008329fac315c238ece33f
Sid <1@2.ru>
2021-06-02 13:10:48 +0400

    CoursesPage. Add materialize.css

--- src/components/Pages/Courses/CoursePageItem.tsx
+++ src/components/Pages/Courses/CoursePageItem.tsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React from "react";
 
 type PropsType = {
-  name: string
-  value: number
-  ticker: string
-  nominal: number
+  name: string;
+  value: number;
+  ticker: string;
+  nominal: number;
+  previousValue: number
+};
+
+const ArrowIcon: React.FC<{diff: number}> = ({ diff }) => {
+  if(diff < 0) {
+    return <>
+       <i className="material-icons red-text">arrow_drop_up</i> 
+    </>
+  }
+  return <>
+  <i className="material-icons green-text">arrow_drop_down</i> 
+  </>
 }
 
-export const CoursePageItem: React.FC<PropsType> = ({name, value, ticker, nominal}) => {
-  return <>
-    <div>
-      <span> {nominal} {name}
-       
-      {(nominal>1) ? " равны " : " равен "}
-      <strong> {value} </strong> рублям </span>   
-    </div>
-  </>
+export const CoursePageItem: React.FC<PropsType> = ({
+  name,
+  value,
+  ticker,
+  nominal,
+  previousValue
+}) => {
+  const diff: number = +(previousValue - value).toFixed(2)
+
+  return (
+    <tr>
+      <td> {ticker} </td>
+      <td> {nominal} </td>
+      <td> {name} </td>
+      <td> <b> {(value).toFixed(2)} </b> </td>
+      <td> {(value - previousValue).toFixed(2)}</td>
+      <td><ArrowIcon diff={diff}/></td>
+    </tr>
+  )
 }
--- src/components/Pages/Courses/CoursesPage.tsx
+++ src/components/Pages/Courses/CoursesPage.tsx
@@ -1,50 +1,92 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCourses } from "../../../Redux/coursesPageReducer";
 import { AppStateType } from "../../../Redux/store";
-import { Valutes } from '../../../Redux/types';
-import { CoursePageItem } from './CoursePageItem';
-
+import { Valutes } from "../../../Redux/types";
+import { CoursePageItem } from "./CoursePageItem";
 
 export const Courses = () => {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  async function  getCurrentCourses() {
-    dispatch(getCourses())
+  async function getCurrentCourses() {
+    dispatch(getCourses());
   }
 
-  const valutes: Valutes = useSelector( (state:AppStateType) => state.coursesPage.valutes)
-  const date: string = useSelector ( (state:AppStateType) => state.coursesPage.date)
-
-
-  useEffect( () => {
-    console.log(valutes)
-  }, [valutes])
-  
-  const keys = Object.keys(valutes)
-
-  return (
-    <>
-      <h4>Курс валют</h4>
-      <p>На дату: <strong>{date}</strong></p>
-
-      <ul>
-          {keys.map( (i) => {
-          // return <li> {valutes[i].Name}  {valutes[i].Value} key={Date.now()}</li>
-          return <CoursePageItem
-           name={valutes[i].Name}
-           value={valutes[i].Value}
-           ticker={valutes[i].CharCode}
-           nominal={valutes[i].Nominal}
-           
-           key={valutes[i].ID}
-
-          />
-        })}
-      </ul>
-      
-      <button className="btn" onClick = {getCurrentCourses}>Get Courses</button>
-    </>
+  const valutes: Valutes = useSelector(
+    (state: AppStateType) => state.coursesPage.valutes
   );
+  const date: string = useSelector(
+    (state: AppStateType) => state.coursesPage.date
+  );
+
+  useEffect(() => {
+    console.log(valutes);
+  }, [valutes]);
+
+
+  
+const dateOptions:any = {
+  era: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  weekday: 'long',
+  timezone: 'UTC+3',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric'
+};
+
+  const keys = Object.keys(valutes);
+  const formatDate:any = new Date(Date.parse(date)).toLocaleString("ru", dateOptions)
+   
+  if (keys.length > 0) {
+    return (
+      <>
+        <h4>Курс валют</h4>
+        <p>
+          На дату: <strong>{formatDate}</strong>
+      
+        </p>
+        <table className="striped">
+          <thead>
+            <tr>
+              <th>Тикер</th>
+              <th>Номинал</th>
+              <th>Наименование</th>
+              <th>Рублей</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {keys.map((i) => {
+              // return <li> {valutes[i].Name}  {valutes[i].Value} key={Date.now()}</li>
+              return (
+                <CoursePageItem
+                  name={valutes[i].Name}
+                  value={valutes[i].Value}
+                  ticker={valutes[i].CharCode}
+                  nominal={valutes[i].Nominal}
+                  previousValue={valutes[i].Previous}
+                  key={valutes[i].ID}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+
+        <button className="btn" onClick={getCurrentCourses}>
+          Get Courses
+        </button>
+      </>
+    );
+  } else
+    return (
+      <>
+        <h4>Курс валют</h4>
+        <button className="btn" onClick={getCurrentCourses}>
+          Get Courses
+        </button>
+      </>
+    );
 };
